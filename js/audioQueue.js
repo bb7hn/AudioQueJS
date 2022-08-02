a55ee0413b7127e1d93cb30af91e11ce01105cec
Batuhan Özen <batuhanozen06@gmail.com>
2022-08-03 00:25:27 +0300

    bufixing
    update module

--- js/audioQueue.js
+++ js/audioQueue.js
@@ -1,7 +1,7 @@
 let lastSong = false;
 /**
  * @param {array} fileName fileName must to be fileName without extension.
- * @param {bool} loop loop is an optional parameter. If you want to play que reversed you can set it true.
+ * @param {bool} loop loop is an optional parameter. If you want to play queue reversed you can set it true.
  * @param {string} sourceUrl sourceUrl is an optional parameter. If you wanna use external url you can set sth. like 'https:example.com/'. It's default is main dir of website ('/).
  */
 const play = async (fileName,loop=false,sourceUrl = "/")=>{
@@ -11,31 +11,31 @@ const play = async (fileName,loop=false,sourceUrl = "/")=>{
     return audio;
 }
 /**
- * @param {array} que Que must to be array of audio urls.
- * @param {bool} reversed reversed is an optional parameter. If you want to play que reversed you can set it true.
+ * @param {array} queue queue must to be array of audio urls.
+ * @param {bool} reversed reversed is an optional parameter. If you want to play queue reversed you can set it true.
  * @param {string} sourceUrl sourceUrl is an optional parameter. If you wanna use external url you can set sth. like 'https:example.com/'. It's default is main dir of website ('/).
  */
-const audioQue = async (que,reversed=false,sourceUrl='/')=>{
+const audioQueue = async (queue,reversed=false,sourceUrl='/')=>{
     //if you dont' want to delete original array must to use like this.
     //i guess this method called as pass by value
-    que = [...que];
+    queue = [...queue];
     if(!reversed){
-        que = que.reverse();
+        queue = queue.reverse();
     }
-    if(que.length != 0){
-        let lastIdx = que.length - 1;
-        let music = que[lastIdx]
+    if(queue.length != 0){
+        let lastIdx = queue.length - 1;
+        let music = queue[lastIdx]
         if(lastSong != false){
             lastSong.addEventListener("ended", async() => {
                 lastSong = await play(music,false,sourceUrl);
-                que.pop();
-                audioQue(que,true,sourceUrl);
+                queue.pop();
+                audioQueue(queue,true,sourceUrl);
             });
         }
         else{
             lastSong = await play(music,false,sourceUrl);
-            que.pop();
-            audioQue(que,true,sourceUrl);
+            queueue.pop();
+            audioQueue(queueue,true,sourceUrl);
         }
     }
     else{
@@ -43,4 +43,4 @@ const audioQue = async (que,reversed=false,sourceUrl='/')=>{
     }
     
 }
-export {audioQue,play};
+export {audioQueue,play};
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Purpose And Presentation
-I created this module because I read some blog posts about **why you can't create an audio (playlist) que with JS**. Then I wanted to try 😊.
-Soooo... , here I am with audioQueJs
+I created this module because I read some blog posts about **why you can't create an audio queue (playlist) with JS**. Then I wanted to try 😊.
+Soooo... , here I am with audioQueueJS
 ## Functions
 ### play() :
 | Parameter   | Description | Required    |
@@ -8,11 +8,11 @@ Soooo... , here I am with audioQueJs
 |**fileName** (*string*)| file name of the audio (can contain folder path too). e.g. : 'audio/testAudios/audio.mp3'|true|
 |**loop** (*boolean*)| it sets will the audio loop or don't. e.g. true (default is *false*)|false|
 |**sourceUrl** (*string*)| full path of the website which contains audio files. e.g. 'https://batuhanozen.com' (default is *'/'*)|false|
-### audioQue() :
+### audioQueue() :
 | Parameter   | Description | Required    |
 | ----------- | ----------- | ----------- |
-|**que** (*string*)| file name of the audio (can contain folder path too). e.g. : 'audio/testAudios/audio.mp3'|true|
-|**reversed** (*boolean*)| it sets will the que start from the end or from the beginning. e.g. true (default is *false*)|false|
+|**queue** (*string*)| file name of the audio (can contain folder path too). e.g. : 'audio/testAudios/audio.mp3'|true|
+|**reversed** (*boolean*)| it sets will the queue start from the end or from the beginning. e.g. true (default is *false*)|false|
 |**sourceUrl** (*string*)| full path of the website which contains audio files. e.g. 'https://batuhanozen.com' (default is *'/'*)|false|
-### [Demo](https://bb7hn.github.io/audioQueJS/)
+### [Demo](https://bb7hn.github.io/audioQueueJS/)
 #### Author and Contributor : [Batuhan ÖZEN](https://batuhanozen.com)
